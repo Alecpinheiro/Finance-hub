0a0ba83c4404cbe32027391541f74dd6c6a9cfc2
Alexandre Pinheiro <alexandrepinheiro@alexandres-mbp.speedport.ip>
2022-12-19 15:41:12 +0100

    refactoring

--- project/__init__.py
+++ project/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 app.debug = True
 
-from project.users.controller import *
+from project.users.users_controller import *
 from project.categories.cat_controller import *
 from project.currencies.currencies_controller import *
 from project.transaction_type.transaction_controller import *
+from project.category_subtype.sub_cat_controller import *
